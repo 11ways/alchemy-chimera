0d5dfb0214f719d4715628e6d62a029df57009e7
Jelle De Loecker <jelle@codedor.be>
2014-03-17 11:35:56 +0100

    Fix timepickers not being inited and incomplete assoc data

--- assets/scripts/chimera-admin.js
+++ assets/scripts/chimera-admin.js
@@ -1,5 +1,57 @@
 (function() {
 
+var timeOptions = {
+	pickDate: true,
+	pickTime: true,
+	pick12HourFormat: false,
+	format: 'YYYY/MM/DD HH:mm',
+	icons: {
+		time: 'fa fa-clock-o',
+		date: 'fa fa-calendar',
+		up: 'fa fa-arrow-up',
+		down: 'fa fa-arrow-down'
+	}
+};
+
+// Apply datepicker
+hawkejs.spot.introduced('hawkejs[data-chimera-input] input.datepicker', function(elements) {
+
+	var $pickers = $(elements);
+
+	$pickers.each(function() {
+
+		var $this   = $(this),
+		    $hidden = $this.siblings('input[type="hidden"]'),
+		    value   = $this.val(),
+		    date    = new Date(value);
+
+		// Set the datetimepicker
+		$this.datetimepicker(timeOptions);
+		$this.data('DateTimePicker').setDate(date);
+
+		// Enable the hidden field
+		$hidden.attr('name', $this.attr('name'));
+
+		if (date.valueOf()) {
+			// Set the beginning value on the hidden field
+			$hidden.val(date.toISOString());
+		}
+
+		// Make sure the datepicker field value doesn't get submitted
+		$this.attr('name', '');
+
+		// Make sure this doesn't get picked again
+		//$this.addClass('picked');
+	});
+
+	$pickers.on('change.dp', function(e) {
+		var $this   = $(this),
+		    $hidden = $this.siblings('input[type="hidden"]');
+
+		$hidden.val(e.date.toISOString());
+	});
+});
+
 $(document).ready(function() {
 
 	// @todo: integrate into i18n
@@ -39,56 +91,7 @@ hawkejs.event.on({create: 'implementation', name: 'chimera/model_editor_filters'
 
 });
 
-hawkejs.event.on({create: 'block'}, function(query, payload) {
-	
-	var timeOptions = {
-		pickDate: true,
-		pickTime: true,
-		pick12HourFormat: false,
-		format: 'YYYY/MM/DD HH:mm',
-		icons: {
-			time: 'fa fa-clock-o',
-			date: 'fa fa-calendar',
-			up: 'fa fa-arrow-up',
-			down: 'fa fa-arrow-down'
-		}
-	};
-	
-	var $pickers = $('[data-hawkejs-block="content"] input.datepicker').not('.picked');
 
-	$pickers.each(function() {
-
-		var $this   = $(this),
-		    $hidden = $this.siblings('input[type="hidden"]'),
-		    value   = $this.val(),
-		    date    = new Date(value);
-
-		// Set the datetimepicker
-		$this.datetimepicker(timeOptions);
-		$this.data('DateTimePicker').setDate(date);
-
-		// Enable the hidden field
-		$hidden.attr('name', $this.attr('name'));
-
-		if (date.valueOf()) {
-			// Set the beginning value on the hidden field
-			$hidden.val(date.toISOString());
-		}
-
-		// Make sure the datepicker field value doesn't get submitted
-		$this.attr('name', '');
-
-		// Make sure this doesn't get picked again
-		$this.addClass('picked');
-	});
-
-	$pickers.on('change.dp', function(e) {
-		var $this   = $(this),
-		    $hidden = $this.siblings('input[type="hidden"]');
-
-		$hidden.val(e.date.toISOString());
-	});
-});
 
 // Enable nanoscroller
 hawkejs.event.on({create: 'block', name: 'admin-main'}, function(query, payload) {
@@ -485,7 +488,7 @@ function applyChimeraFields(query, payload) {
 				}
 
 				if (id) {
-					$.post(assocUrl, {id: id}, function(data) {
+					$.post(assocUrl, {init: true, id: id}, function(data) {
 						if (multiple) {
 							callback(data.results)
 						} else {
