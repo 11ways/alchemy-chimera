46ba983634d495c76cf0da33147da3db119faf49
Kjell Keisse <kjell@codedor.be>
2014-10-20 11:49:56 +0200

    old chimera.js functions have been converted

--- assets/scripts/chimera/chimera.js
+++ assets/scripts/chimera/chimera.js
@@ -190,6 +190,101 @@ GeopointChimeraField.setMethod(function initList() {
 	result = applyGeopoint($input, coordinates[0], coordinates[1], options);
 });
 
+function applyGeopoint($el, lat, lng, _options) {
+
+	var markOptions,
+	    options,
+	    marker,
+	    lat,
+	    lng,
+	    map;
+
+	if ($el == null) {
+		throw new Error('Wrapper element not found, can\'t create map');
+	}
+
+	// Skip this map if the coordinates are not numbers
+	if (!isFinite(lat) || !isFinite(lng)) {
+		lat = 51.044821;
+		lng = 3.738785;
+	}
+
+	options = {
+		dragging: false,
+		touchZoom: false,
+		center: [lat+0.0012, lng],
+		zoomControl: false,
+		attributionControl: false,
+		scrollWheelZoom: 'center',
+		minZoom: 13,
+		maxZoom: 15,
+		zoom: 14
+	};
+
+	Object.assign(options, _options);
+
+	// Add the point to the map
+	map = L.map($el[0], options);
+
+	L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+		attribution: '',
+		maxZoom: 16
+	}).addTo(map);
+
+	markOptions = {};
+
+	if (options.editable === true) {
+		markOptions.draggable = true;
+	}
+
+	marker = L.marker([lat, lng], markOptions).addTo(map);
+
+	return [map, marker];
+}
+
+
+/**
+ * The Text ChimeraField class
+ *
+ * @constructor
+ *
+ * @author   Kjell Keisse   <kjell@codedor.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ *
+ * @param    {DOMElement}   container
+ * @param    {Object}       variables
+ */
+TextChimeraField = ChimeraField.extend(function TextChimeraField(container, variables) {
+	TextChimeraField.super.call(this, container, variables);
+});
+
+/**
+ * Initialize the field
+ *
+ * @param    {Mixed}   value
+ */
+TextChimeraField.setMethod(function initEdit() {
+	
+	var that = this,
+	    name,
+	    editor, id;
+
+	name = '.medium-editor';
+	id = $(name, this.intake)[0].id;
+	
+	editor = new MediumEditor(name, {
+		buttons: ['bold', 'italic', 'underline', 'strikethrough', 'anchor', 'image', 'header1', 'header2', 'quote', 'pre']
+	});
+
+	$(name).on('input', function onTextEdit(){
+		if($(this)[0].id === id){
+			that.setValue($(this)[0].innerHTML);
+		}
+	});
+
+});
+
 
 hawkejs.scene.on({type: 'set', name: 'pageCentral', template: 'chimera/editor/edit'}, applySave);
 hawkejs.scene.on({type: 'set', name: 'pageCentral', template: 'chimera/editor/add'}, applySave);
@@ -260,167 +355,128 @@ function applySave(el, variables) {
 }
 
 /**
- * Old code
+ * The Boolean ChimeraField class
+ *
+ * @constructor
+ *
+ * @author   Kjell Keisse   <kjell@codedor.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ *
+ * @param    {DOMElement}   container
+ * @param    {Object}       variables
  */
+BooleanChimeraField = ChimeraField.extend(function BooleanChimeraField(container, variables) {
+	BooleanChimeraField.super.call(this, container, variables);
+});
 
-hawkejs.scene.on({type: 'create', template: 'chimera/field_wrappers/geopoint_list'}, listGeopoint);
-hawkejs.scene.on({type: 'create', implement: 'chimera/fields/geopoint_view'}, listGeopoint);
-hawkejs.scene.on({type: 'create', implement: 'chimera/fields/geopoint_edit'}, editGeopoint);
-hawkejs.scene.on({type: 'create', implement: 'chimera/fields/text_edit'}, editText);
-
-function editText(el, block) {
-
-	var $el = $(el),
-	    name,
-	    editor, id;
-
-	name = '.medium-editor';
-	id = el.getElementsByClassName('medium-editor')[0].id;
+/**
+ * Initialize the field
+ *
+ * @param    {Mixed}   value
+ */
+BooleanChimeraField.setMethod(function initEdit() {
 	
-	editor = new MediumEditor(name, {
-		buttons: ['bold', 'italic', 'underline', 'strikethrough', 'anchor', 'image', 'header1', 'header2', 'quote', 'pre']
-	});
-
-	$(name).on('input', function onTextEdit(){
-		if($(this)[0].id === id){
-			$el.data('new-value', $(this)[0].innerHTML);
-		}
-	});
-
-}
-
-
-function listGeopoint(el, block) {
-
-	var options,
-	    map;
-
-	options = {
-		dragging: false
-	};
-
-	map = applyGeopoint(el, options);
-}
-
-function editGeopoint(el, block) {
-
-	var options,
-	    result,
-	    marker,
-	    map,
-	    $el;
-
-	$el = $(el);
-
-	options = {
-		minZoom: 1,
-		maxZoom: 16,
-		dragging: true,
-		editable: true
-	};
-
-	result = applyGeopoint(el, options);
-
-	if (!result) {
-		throw new Error('Map wrapper could not be created');
-	}
-
-	map = result[0];
-	marker = result[1];
-
-	marker.on('dragend', function afterDrag() {
-		var coordinates = marker.getLatLng();
-		$el.data('new-value', [coordinates.lat, coordinates.lng]);
-	});
-}
-
-function applyGeopoint($el, lat, lng, _options) {
-
-	var markOptions,
-	    options,
-	    marker,
-	    lat,
-	    lng,
-	    map;
-
-	if ($el == null) {
-		throw new Error('Wrapper element not found, can\'t create map');
-	}
-
-	// Skip this map if the coordinates are not numbers
-	if (!isFinite(lat) || !isFinite(lng)) {
-		lat = 51.044821;
-		lng = 3.738785;
-	}
-
-	options = {
-		dragging: false,
-		touchZoom: false,
-		center: [lat+0.0012, lng],
-		zoomControl: false,
-		attributionControl: false,
-		scrollWheelZoom: 'center',
-		minZoom: 13,
-		maxZoom: 15,
-		zoom: 14
-	};
-
-	Object.assign(options, _options);
-
-	// Add the point to the map
-	map = L.map($el[0], options);
-
-	L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-		attribution: '',
-		maxZoom: 16
-	}).addTo(map);
-
-	markOptions = {};
-
-	if (options.editable === true) {
-		markOptions.draggable = true;
-	}
-
-	marker = L.marker([lat, lng], markOptions).addTo(map);
-
-	return [map, marker];
-}
-
-hawkejs.scene.on({type: 'create', implement: 'chimera/fields/default_edit'}, function editDefault(el) {
-
-	var $el = $(el),
-	    $input = $('.chimeraEditor-input', $el);
-
-	$input.change(function onDefaultEdit() {
-		$el.data('new-value', $input.val());
-	});
-});
-
-hawkejs.scene.on({type: 'create', implement: 'chimera/fields/boolean_edit'}, function editBoolean(el) {
-
-	var $el = $(el),
-	    $input = $('.chimeraEditor-input', $el);
-
+	var that = this,
+	    $input = $('.chimeraEditor-input', this.intake);
+	
 	$input.change(function onBooleanEdit() {
-		$el.data('new-value', $input.is(':checked'));
+		that.setValue($input.is(':checked'));
 	});
+
 });
 
-hawkejs.scene.on({type: 'create', implement: 'chimera/fields/datetime_edit'}, function editDatetime(el) {
-	applyDateField(el, 'datetime');
+/**
+ * The Datetime ChimeraField class
+ *
+ * @constructor
+ *
+ * @author   Kjell Keisse   <kjell@codedor.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ *
+ * @param    {DOMElement}   container
+ * @param    {Object}       variables
+ */
+DatetimeChimeraField = ChimeraField.extend(function DatetimeChimeraField(container, variables) {
+	DatetimeChimeraField.super.call(this, container, variables);
 });
 
-hawkejs.scene.on({type: 'create', implement: 'chimera/fields/date_edit'}, function editDate(el) {
-	applyDateField(el, 'date', {time: false});
+/**
+ * Initialize the field
+ *
+ * @param    {Mixed}   value
+ */
+DatetimeChimeraField.setMethod(function initEdit() {
+	
+	var that = this;
+	
+	applyDateField(that, 'datetime');
+
 });
 
-hawkejs.scene.on({type: 'create', implement: 'chimera/fields/time_edit'}, function editTime(el) {
-	applyDateField(el, 'time', {date: false});
+/**
+ * The Date ChimeraField class
+ *
+ * @constructor
+ *
+ * @author   Kjell Keisse   <kjell@codedor.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ *
+ * @param    {DOMElement}   container
+ * @param    {Object}       variables
+ */
+DateChimeraField = ChimeraField.extend(function DateChimeraField(container, variables) {
+	DateChimeraField.super.call(this, container, variables);
 });
 
-function applyDateField(el, type, options) {
+/**
+ * Initialize the field
+ *
+ * @param    {Mixed}   value
+ */
+DateChimeraField.setMethod(function initEdit() {
+	
+	var that = this;
+	
+	applyDateField(that, 'date', {time: false});
 
-	var $el = $(el),
+});
+
+/**
+ * The Time ChimeraField class
+ *
+ * @constructor
+ *
+ * @author   Kjell Keisse   <kjell@codedor.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ *
+ * @param    {DOMElement}   container
+ * @param    {Object}       variables
+ */
+TimeChimeraField = ChimeraField.extend(function TimeChimeraField(container, variables) {
+	TimeChimeraField.super.call(this, container, variables);
+});
+
+/**
+ * Initialize the field
+ *
+ * @param    {Mixed}   value
+ */
+TimeChimeraField.setMethod(function initEdit() {
+	
+	var that = this;
+	
+	applyDateField(that, 'time', {date: false});
+
+});
+
+
+function applyDateField(that, type, options) {
+
+	var $el = that.intake,
 	    $wrapper = $('.chimeraEditor-' + type + '-edit', $el),
 	    calender,
 	    value;
@@ -438,12 +494,12 @@ function applyDateField(el, type, options) {
 
 	calender.on('data', function dateChange(dateString) {
 		var newdate = calender.getDate();
-		$el.data('new-value', newdate);
+		that.setValue(newdate);
 	});
 }
 
 
-hawkejs.scene.on({type: 'create', implement: 'chimera/sidebar'}, sidebarCollapse);
+hawkejs.scene.on({type: 'set', template: 'chimera/sidebar'}, sidebarCollapse);
 
 function sidebarCollapse(el) {
 
