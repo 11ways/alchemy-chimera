60fd96b6683ccde7820aff8324d056f046702d08
Jelle De Loecker <jelle@develry.be>
2019-05-08 11:32:57 +0200

    Add success or error states to password fields

--- assets/scripts/chimera/password_field.js
+++ assets/scripts/chimera/password_field.js
@@ -18,37 +18,56 @@ var PasswordChimeraField = ChimeraField.extend(function PasswordChimeraField(par
 });
 
 /**
- * Create the edit input element
- *
- * @author   Jelle De Loecker   <jelle@kipdola.be>
- * @since    0.2.0
- * @version  0.2.0
- */
-PasswordChimeraField.setMethod(function renderEdit() {
-
-	var html = '<input class="chimeraField-string chimeraPassword-first" type="password" placeholder="Enter a new password">';
-	html += '<input class="chimeraField-string chimeraPassword-second" type="password" placeholder="Repeat the same password">';
-
-	this.setMainElement(html);
-});
-
-/**
  * Initialize the field
  *
  * @param    {Mixed}   value
  */
 PasswordChimeraField.setMethod(function initEdit() {
 
+	"use strict";
+
 	var that = this,
-	    $first = $('.chimeraPassword-first', this.intake),
-	    $second = $('.chimeraPassword-second', this.intake);
+	    $first = $('.chimeraPassword-first', this.container),
+	    $second = $('.chimeraPassword-second', this.container),
+	    $validation = $('.validation', this.container);
 
 	$first.add($second).change(function onFirstChange() {
+
+		checkMatch();
 
 		if ($first.val() == $second.val()) {
 			that.setValue($first.val());
 		} else {
-			that.setValue(null);
+			//that.setValue(null);
 		}
 	});
+
+	$first.add($second).on('keyup', function() {
+		checkMatch();
+	});
+
+	function checkMatch() {
+
+		var first = $first.val(),
+		    second = $second.val();
+
+		if (first == second) {
+			$validation.text('');
+			that.container.classList.remove('has-error');
+
+			if (first) {
+				that.container.classList.add('has-success');
+			}
+		} else {
+			let error_html = String(alchemy.__('chimera', 'passwords.dont.match'));
+			let html = '<br><div class="alert alert-danger" role="alert">'
+				+ '<i class="alert-ico fa fa-fw fa-ban"></i>'
+				+ error_html
+				+ '</div>';
+
+			$validation.html(html);
+			that.container.classList.add('has-error');
+			that.container.classList.remove('has-success');
+		}
+	}
 });
